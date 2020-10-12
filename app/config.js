d33cef7c15216f366b2d2918cb66d4bf9c736174
Dominique Sherwood <dominique.sherwood@education.gov.uk>
2020-10-12 21:05:35 +0100

    Almost there with supplier

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Early Career Framework',
+  serviceName: 'Register for the Early Career Framework',
 
   // Default port that prototype runs on
   port: '3000',
